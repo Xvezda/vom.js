5e518732a046ecd5b358f83f318a34bd775edcbf
Xvezda <xvezda@naver.com>
2021-12-14 15:23:54 +0900

    chore: Webpack 설정 파일 분리

---
+++ packages/vomjs/webpack.prod.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./webpack.common.js');
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      'NODE_ENV': 'production',
+    }),
+  ],
+});
