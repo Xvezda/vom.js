79eb02ecaedfc199fad6dfad2edbd2758e365bc0
Xvezda <xvezda@naver.com>
2021-12-14 15:29:55 +0900

    chore: Production 빌드용 sourcemap 추가

--- packages/vomjs/webpack.prod.js
+++ packages/vomjs/webpack.prod.js
@@ -5,6 +5,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   plugins: [
     new webpack.EnvironmentPlugin({
       'NODE_ENV': 'production',
