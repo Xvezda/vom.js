8d55bf9d7fe2f9f3855f18ca6813564af947a6d1
Xvezda <xvezda@naver.com>
2021-12-14 15:57:11 +0900

    refactor: Prototype 상속을 class 형태로 변경

--- packages/vomjs-tools/src/pattern-match.js
+++ packages/vomjs-tools/src/pattern-match.js
@@ -1,48 +1,52 @@
 import { deepEquals, wildcard } from './common.js';
+export {
+  wildcard as _,
+  deepEquals as exact,
+} from './common.js';
 
+class PatternMatch {
+  constructor(target) {
+    this.$target = target;
+    this.$result = undefined;
+    this.$matched = false;
+  }
 
-function PatternMatch(target) {
-  this.$target = target;
-  this.$result = undefined;
-  this.$matched = false;
-}
+  static match(target) {
+    return new PatternMatch(target);
+  }
 
-PatternMatch.match = function (target) {
-  return new PatternMatch(target);
-};
+  when(pattern, ifMatch) {
+    if (this.$matched) {
+      return this;
+    } else if (pattern === wildcard) {
+      return this.otherwise(ifMatch);
+    }
+    const flag = (typeof pattern === 'function' && pattern(this.$target)) ||
+      (pattern instanceof RegExp && pattern.test(this.$target)) ||
+      (deepEquals(pattern, this.$target));
 
-PatternMatch.prototype.when = function (pattern, ifMatch) {
-  if (this.$matched) {
+    if (flag && typeof this.$result === 'undefined') {
+      this.$matched = true;
+      if (typeof ifMatch === 'function') {
+        this.$result = ifMatch(this.$target);
+      } else {
+        this.$result = ifMatch;
+      }
+    }
     return this;
-  } else if (pattern === wildcard) {
-    return this.otherwise(ifMatch);
   }
-  const flag = (typeof pattern === 'function' && pattern(this.$target)) ||
-    (pattern instanceof RegExp && pattern.test(this.$target)) ||
-    (Object.keys(pattern).length > 0 && deepEquals(pattern, this.$target)) ||
-    (deepEquals(pattern, this.$target));
 
-  if (flag && typeof this.$result === 'undefined') {
-    this.$matched = true;
-    if (typeof ifMatch === 'function') {
-      this.$result = ifMatch(this.$target);
-    } else {
-      this.$result = ifMatch;
+  otherwise(fallback) {
+    if (!this.$matched && fallback) {
+      if (typeof fallback === 'function') {
+        this.$result = fallback();
+      } else {
+        this.$result = fallback;
+      }
     }
+    return this.$result;
   }
-  return this;
-};
-
-PatternMatch.prototype.otherwise = function (otherwise) {
-  if (!this.$matched && otherwise) {
-    if (typeof otherwise === 'function') {
-      this.$result = otherwise();
-    } else {
-      this.$result = otherwise;
-    }
-  }
-  return this.$result;
-};
+}
 
 export const match = PatternMatch.match;
 export default PatternMatch;
