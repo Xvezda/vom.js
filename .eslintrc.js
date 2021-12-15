04013a937f0efe8b54b7484102b061d574e263f3
Xvezda <xvezda@naver.com>
2021-12-15 17:21:49 +0900

    chore: eslint 설정 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true,
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 13,
+    "sourceType": "module"
+  },
+  "ignorePatterns": ["**/dist/*.js"],
+  "rules": {
+    "semi": "error",
+    "quotes": ["error", "single"],
+    "indent": [
+      "error", 2, {
+        "MemberExpression": "off",
+        "SwitchCase": 1,
+        "CallExpression": {"arguments": 1},
+        "VariableDeclarator": "first",
+        "ignoredNodes": [
+          "TaggedTemplateExpression[tag.name='html'] *",
+          "MemberExpression CallExpression[arguments]"
+        ],
+      }
+    ],
+    "max-len": [
+      "error", {
+        "code": 80,
+        "ignoreComments": true,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true,
+      }
+    ]
+  }
+};
