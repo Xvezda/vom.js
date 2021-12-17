d48c4b76e220c3d3a2d1f96fd6b6870aef8ea28e
Xvezda <xvezda@naver.com>
2021-12-17 20:09:07 +0900

    test: `selector` 테스트 추가

---
+++ packages/vomjs-tools/src/selector.spec.js
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+import { select } from './selector';
+
+describe('select', () => {
+  const div = document.createElement('div');
+  div.id = 'dummy';
+  document.body.appendChild(div);
+
+  test('selector', () => {
+    expect(select('#dummy').context()).toBe(div);
+  });
+
+  const button = document.createElement('button');
+  button.textContent = 'click me';
+  document.body.appendChild(button);
+
+  test('on and off', () => {
+    const mock = jest.fn();
+    const selected = select(button).on('click', mock);
+
+    expect(selected.context()).toBe(button);
+
+    expect(mock).not.toHaveBeenCalled();
+    button.click();
+    expect(mock).toHaveBeenCalled();
+
+    button.click();
+    expect(mock).toHaveBeenCalledTimes(2);
+
+    selected.off('click', mock);
+    button.click();
+    expect(mock).toHaveBeenCalledTimes(2);
+  });
+});
