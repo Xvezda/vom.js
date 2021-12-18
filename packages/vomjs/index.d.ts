fa5085c6edb436871d5a8d3f543823c4792b2c53
Xvezda <xvezda@naver.com>
2021-12-18 09:25:40 +0900

    chore: 추가 API 타입 선언 및 `Reference` 제네릭 사용

--- packages/vomjs/index.d.ts
+++ packages/vomjs/index.d.ts
@@ -12,8 +12,8 @@ export interface RenderProps {
   render: Component | (() => Template);
 }
 
-export type Reference = {
-  current: any;
+export type Reference<T> = {
+  current: T;
 };
 
 export function render(component: Component, element: HTMLElement | null): void;
@@ -21,9 +21,10 @@ export function html(strings: TemplateStringsArray, ...args: any[]): Template;
 export function bind(component: Component): <T>(attrs: T) => Component;
 export function useState<T>(state: T): [state: T, setter: (newState: T | ((prev: T) => T)) => void];
 export function useEffect(effect: () => void, deps?: any[]): void;
-export function useRef(): Reference;
-export function useEventListener(ref: Reference, eventName: string, handler: EventListener): void;
-export function useDelegation(eventName: string, handler: (target: HTMLElement, event: Event) => void): Reference;
+export function useLayoutEffect(effect: () => void, deps?: any[]): void;
+export function useRef<T>(): Reference<T>;
+export function useEventListener<T>(ref: Reference<T>, eventName: string, handler: EventListener): void;
+export function useDelegation<T>(eventName: string, handler: (target: HTMLElement, event: Event) => void): Reference<T>;
 export function useMemo<T>(toEvaluate: () => T, deps?: any[]): T;
 export function useCallback<T>(callback: T, deps?: any[]): T;
 export function useReducer<State, Action extends { type: string, payload?: unknown }>(
@@ -32,3 +33,4 @@ export function useReducer<State, Action extends { type: string, payload?: unkno
 export function useReducer<State, Action extends { type: string, payload?: unknown }>(
   reducer: (state: State, action: Action) => State, initialArg: any, init?: (value: any) => State
   ): [State, (action: Action) => State];
+export function useImperativeHandle<T>(ref: Reference<T>, createHandle: () => T, deps?: any[]): void;
