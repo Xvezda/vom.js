fe91f6dcec107d88e75dfc0fd691e42f30cbcc46
Xvezda <xvezda@naver.com>
2021-12-18 14:38:46 +0900

    perf: 비교 알고리즘 최적화

--- packages/vomjs-tools/src/common.js
+++ packages/vomjs-tools/src/common.js
@@ -1,21 +1,22 @@
 export const wildcard = Object.create(null);
 export function deepEquals(a, b) {
   if (
+    a === wildcard || b === wildcard ||
     (a === undefined && b === undefined) ||
     (a === null && b === null)
   ) {
     return true;
   }
 
-  if (Array.isArray(a)) {
-    return Array
-      .from(a)
-      .every((v, i) => {
-        if (v === wildcard) {
-          return b.length > i;
-        }
-        return deepEquals(v, b[i]);
-      });
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length)
+      return false;
+
+    for (let i = 0; i < a.length; ++i) {
+      if (!deepEquals(a[i], b[i]))
+        return false;
+    }
+    return true;
   } else if (
     a && a.constructor === Object &&
     b && b.constructor === Object
