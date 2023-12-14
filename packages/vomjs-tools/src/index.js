a26f7dfcc7e5a24439365d7ec95b66f54749197d
Xvezda <xvezda@naver.com>
2023-12-15 00:40:42 +0900

    chore: export 전용 파일 ignore 처리

--- packages/vomjs-tools/src/index.js
+++ packages/vomjs-tools/src/index.js
@@ -1,5 +1,7 @@
+/* v8 ignore start */
 export * from './common.js';
 export * from './selector.js';
 export * from './pattern-match.js';
 export * from './fp.js';
 export * from './optimize.js';
+/* v8 ignore stop */
